cc20c4a2902a7ed7d4dd552d958c43c3e81c5a10
Anwesh265 <anweshsriramoju98@gmail.com>
2025-01-23 12:06:51 +0530

    This is my task related to team34 Hybrid project

---
+++ src/test/java/com_TestCases/Example01.java
@@ -0,0 +1,13 @@
+package com_TestCases;
+
+public class Example01 {
+	
+	public static void main(String[] args) {
+		
+		 int a = 20;
+		 int b = 10;
+		 int c = 30;
+		 System.out.println(a+b+c);
+	}
+
+}
